20e2e537be412a1072ce0a2d578037bdb684ef31
turleo <leonid.tureyskiy@ya.ru>
2022-02-13 16:08:46 +1000

    now websocket can log out

--- wsapi/consumer.py
+++ wsapi/consumer.py
@@ -1,13 +1,15 @@
 import json
 
-from channels.auth import login
-from channels.exceptions import DenyConnection
+from channels.auth import login, logout
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ApiConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        from django.contrib.auth.models import AnonymousUser
         await self.accept()
+        if self.scope["user"] == AnonymousUser():
+            await self.send(json.dumps({"event": "authentication.auth"}))
 
     async def receive(self, text_data=None, bytes_data=None):
         from django.contrib.auth.models import AnonymousUser
@@ -15,12 +17,15 @@ class ApiConsumer(AsyncWebsocketConsumer):
         if self.scope["user"] == AnonymousUser() and message['event'].startswith('authentication'):
             token = callbacks[message['event']](self.scope, **message)
             if token is None:
-                await self.close()
+                await self.send(json.dumps({"event": "authentication.error"}))
                 return
             await login(self.scope, token.user)
             self.scope["session"].save()
-            await self.send(json.dumps({"token": token.token}))
+            await self.send(json.dumps({"event": "authentication.token", "token": token.token}))
         elif self.scope["user"] == AnonymousUser():
+            await self.close()
+        elif message['event'] == 'authentication.logout':
+            await logout(self.scope)
             await self.close()
         else:
             await self.send(callbacks[message['event']](self.scope, **message))
