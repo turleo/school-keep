b3da430cb6b845bbb9c0cb6ec5dca761cfbbb359
turleo <leonid.tureyskiy@ya.ru>
2022-02-25 21:20:47 +1000

    `timetable.lessons.*` renamed to `timetable.bells.*`

--- timetable/bells.py
+++ timetable/bells.py
@@ -6,13 +6,13 @@ import wsapi
 from timetable.models import Bell
 
 
-@wsapi.add_callback("timetable.lessons.fetch")
+@wsapi.add_callback("timetable.bells.fetch")
 def fetch(scope: dict, **kwargs) -> list:
     lessons = Bell.objects.filter(owner=scope['user'])
     return list(map(lambda x: x.toJSON(), lessons))
 
 
-@wsapi.add_callback("timetable.lessons.change")
+@wsapi.add_callback("timetable.bells.change")
 def change(scope: dict, **kwargs):
     bell_data = kwargs.get("data")
     if bell_data.get("id", 0) == 0:
@@ -23,10 +23,10 @@ def change(scope: dict, **kwargs):
         bell.end = bell_data['end']
         bell.days = ",".join(bell_data['days'])
     bell.save()
-    return wsapi.callbacks['timetable.lessons.fetch'](scope, **kwargs)
+    return wsapi.callbacks['timetable.bells.fetch'](scope, **kwargs)
 
 
-@wsapi.add_callback("timetable.lessons.delete")
+@wsapi.add_callback("timetable.bells.delete")
 def delete(scope: dict, **kwargs):
     try:
         bell_id = kwargs.get("id", 0)
@@ -34,4 +34,4 @@ def delete(scope: dict, **kwargs):
         bell.delete()
     except ObjectDoesNotExist:
         pass
-    return wsapi.callbacks['timetable.lessons.fetch'](scope, **kwargs)
+    return wsapi.callbacks['timetable.bells.fetch'](scope, **kwargs)
