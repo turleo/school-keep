c8c181cbc32f601c16ad823a6386f10c6732e0fb
turleo <leonid.tureyskiy@ya.ru>
2022-03-06 18:26:49 +1000

    tasks can be changed too

--- hometask/views.py
+++ hometask/views.py
@@ -1,11 +1,12 @@
 import wsapi
-from hometask.models import Hometask
+from hometask.models import Hometask, Task
 
 
 @wsapi.add_callback("hometask.fetch")
 def get_hometasks(scope: dict, **kwargs):
     hometasks = Hometask.objects.filter(owner=scope['user'])
     return {"event": "hometask", "data": list(map(lambda x: x.toJSON(), hometasks))}
+
 
 @wsapi.add_callback("hometask.change")
 def change_hometasks(scope: dict, **kwargs):
@@ -18,3 +19,14 @@ def change_hometasks(scope: dict, **kwargs):
         hometasks.save()
     return wsapi.callbacks['hometask.fetch'](scope, **kwargs)
 
+
+@wsapi.add_callback("hometask.task.change")
+def change_hometasks(scope: dict, **kwargs):
+    kwargs = kwargs['data']
+    task_id = kwargs.get("id", 0)
+    if task_id != 0:
+        task = Task.objects.get(pk=task_id)
+        task.text = kwargs["text"]
+        task.done = kwargs["done"]
+        task.save()
+    return wsapi.callbacks['hometask.fetch'](scope, **kwargs)
