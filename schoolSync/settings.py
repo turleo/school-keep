260d419f930705d37ddae080992173d0b2779aca
turleo <turleo@outlook.com>
2022-04-17 16:40:28 +1000

    migrations added

--- schoolSync/settings.py
+++ schoolSync/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
+STATICFILES_DIRS = (os.path.join('static'), )
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- schoolSync/urls.py
+++ schoolSync/urls.py
@@ -14,15 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import RedirectView
 
 import authentication.views
+import homepage.urls
 import hometask.views
 import timetable.bells
 import timetable.lessons
 import timetable.subjects
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(homepage.urls)),
 ]
